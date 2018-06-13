f81d1807eb5dcc1110ee9e428d7d9e749cb7cf40
Mengjun Leng <mengju@codeaurora.org>
2018-06-13 17:09:10 +0800

    Fix showing empty signal icon based on CDMA
    If data RAT is IWLAN before signal has been changed,
    Telephony is unable to override the flag isGsm as true.
    
    To fix it, fix the type while switching between LTE and
    IWLAN.
    
    Change-Id: I97cfec52ee93657d490d15e5004d439a18a060d9
    CRs-Fixed: 2255103

--- src/java/com/android/internal/telephony/ServiceStateTracker.java
+++ src/java/com/android/internal/telephony/ServiceStateTracker.java
@@ -3247,6 +3247,12 @@ public class ServiceStateTracker extends Handler {
                             mSS.getRilDataRadioTechnology()), mPhone.getPhoneId());
         }
 
+        if (hasRilVoiceRadioTechnologyChanged || hasRilDataRadioTechnologyChanged) {
+            // Technology has changed, try to fix signal strength type.
+            mSignalStrength.fixType();
+            notifySignalStrength();
+        }
+
         if (hasRegistered) {
             mNetworkAttachedRegistrants.notifyRegistrants();
             mNitzState.handleNetworkAvailable();
